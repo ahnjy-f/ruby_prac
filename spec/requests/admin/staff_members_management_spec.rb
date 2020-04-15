07f65a98fc5f8e7a8a1fd764d4b7e28054efe9f8
ahnjy-f <jeeyoung-ahn@froide.co.jp>
2020-04-15 16:52:57 +0900

    モデル間の関連付け

--- spec/requests/admin/staff_members_management_spec.rb
+++ spec/requests/admin/staff_members_management_spec.rb
@@ -17,8 +17,8 @@ describe "管理者による職員管理" do
             }
     end
 
-    describe "一覧する" do
-        example "成功した場合" do
+    describe "一覧" do
+        example "成功" do
             get admin_staff_members_url
             expect(response.status).to eq(200)
         end
@@ -43,8 +43,8 @@ describe "管理者による職員管理" do
             post admin_staff_members_url, params: { staff_member: params_hash }
             expect(response).to redirect_to(admin_staff_members_url)
         end
-        
-        example "例外ActionController::ParameterMissingが発生" do
+
+        example "例外 ActionController::ParameterMissingが発生" do
             expect { post admin_staff_members_url }.
                 to raise_error(ActionController::ParameterMissing)
         end
@@ -54,7 +54,7 @@ describe "管理者による職員管理" do
         let(:staff_member) { create(:staff_member) }
         let(:params_hash) { attributes_for(:staff_member) }
 
-        example "suspendedフラグをセットする" do
+        example "suspended フラグをセットする" do
             params_hash.merge!(suspended: true)
             patch admin_staff_member_url(staff_member),
                 params: { staff_member: params_hash }
@@ -68,7 +68,7 @@ describe "管理者による職員管理" do
             expect {
                 patch admin_staff_member_url(staff_member),
                     params: { staff_member: params_hash }
-            }.not_to change { staff_member.hashed_password.to_s}
+            }.not_to change { staff_member.hashed_password.to_s }
         end
     end
 end
